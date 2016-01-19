cabb810db7b26151f678abff7523581d5cdd218e
pogopaule <pogopaule@gmx.de>
2016-01-19 14:40:12 +0100

    add demo code

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,5 +3,16 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('foo');
+      //.catch(error => {
+      //// error is 'true' because of https://github.com/simplabs/ember-simple-auth/blob/1.0.1/addon/mixins/data-adapter-mixin.js#L100 :(
+      //debugger;
+    //});
+  },
+
+  actions: {
+    error(error) {
+      // error is 'true' because of https://github.com/simplabs/ember-simple-auth/blob/1.0.1/addon/mixins/data-adapter-mixin.js#L100 :(
+      debugger;
+    }
   }
 });
